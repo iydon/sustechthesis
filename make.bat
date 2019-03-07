c75f6b9a9122c063afd545e154cadb00fa487877
Iydon <1056736800@qq.com>
2019-03-07 20:52:36 +0800

    整理目录
    今晚完工！立flag！

---
+++ make.bat
@@ -0,0 +1,73 @@
+@echo off
+chcp 65001 >nul
+
+set compiler="xelatex"
+set bib="bibtex"
+set filename="main"
+set temp="temp"
+
+if "%1"=="clean" (
+    call:clean
+) else (
+    if "%1"=="show" (
+        call:show
+    ) else (
+        if "%1"=="sure" (
+            call:sure
+        ) else (
+            call:compile
+        )
+    )
+)
+goto:eof
+
+
+rem Function
+
+:compile
+    if exist %temp%/%filename%.bbl (
+        call:pop
+        %compiler% %filename%
+    ) else (
+        %compiler% %filename%
+        %bib%  %filename%
+        call:twice
+    )
+    call:mvto
+goto:eof
+
+:clean
+    del /q %temp%
+    rmdir %temp%
+goto:eof
+
+:mvto
+    if not exist %temp% mkdir %temp%
+    move %filename%.aux %temp% >nul 2>nul
+    move %filename%.bbl %temp% >nul 2>nul
+    move %filename%.blg %temp% >nul 2>nul
+    move %filename%.lof %temp% >nul 2>nul
+    move %filename%.log %temp% >nul 2>nul
+    move %filename%.lot %temp% >nul 2>nul
+    move %filename%.out %temp% >nul 2>nul
+    move %filename%.toc %temp% >nul 2>nul
+goto:eof
+
+:pop
+    move %temp%\* ./
+goto:eof
+
+:twice
+    %compiler% %filename%
+    %compiler% %filename%
+goto:eof
+
+:show
+    %filename%.pdf
+goto:eof
+
+:sure
+    %compiler% %filename%
+    %bib% %filename%
+    call:twice
+goto:eof
